303c18bdcd81df80806325d8dd9b84203a781e59
EimonFarhad <eimonfarhad1996@gmail.com>
2019-04-06 00:46:31 +0530

    First commit

---
+++ app/src/main/java/com/Myproject/myapp/Activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.Myproject.myapp.Activity;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.Myproject.myapp.R;
+import com.Myproject.myapp.fragments.LoginFragment;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+            replaceFrament();
+         }
+
+    private void replaceFrament() {
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.login_continer,new LoginFragment()).commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        replaceFrament();
+    }
+}
---
+++ app/src/main/java/com/Myproject/myapp/fragments/LoginFragment.java
@@ -0,0 +1,55 @@
+package com.Myproject.myapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.Myproject.myapp.R;
+
+public class LoginFragment extends Fragment {
+    Button btn_next;
+    EditText edt_phone,edt_name;
+    TextInputLayout edt_phone_main,edt_name_main;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmentlogin,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initviews(view);
+ btn_next.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View v) {
+         replaceFrgment();
+     }
+ });
+    }
+
+    private void replaceFrgment() {
+        FragmentManager fm=getFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.login_continer,new OtpFragment()).commit();
+    }
+
+    private void initviews(View view) {
+        btn_next=view.findViewById(R.id.btn_next);
+        edt_phone=view.findViewById(R.id.edt_phone);
+        edt_name=view.findViewById(R.id.edt_name);
+        edt_phone_main=view.findViewById(R.id.edt_phone_main);
+        edt_name_main=view.findViewById(R.id.edt_name_main);
+    }
+}
