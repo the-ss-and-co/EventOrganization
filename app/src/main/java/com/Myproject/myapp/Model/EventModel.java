20825f58d6bf67d0f4ed7dd8953d1a5326a67bc0
subhsis <subhasis391@gmail.com>
2019-04-08 00:17:55 +0530

    after adding service and event

--- app/src/main/java/com/Myproject/myapp/Model/EventModel.java
+++ app/src/main/java/com/Myproject/myapp/Model/EventModel.java
@@ -1,4 +1,27 @@
 package com.Myproject.myapp.Model;
 
 public class EventModel {
+    String EventText;
+    int image;
+
+     public EventModel(String eventText, int image){
+        this.EventText= eventText;
+        this.image = image;
+    }
+
+    public String getEventText() {
+        return EventText;
+    }
+
+    public void setEventText(String eventText) {
+        EventText = eventText;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
