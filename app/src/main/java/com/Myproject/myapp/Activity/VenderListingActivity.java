f95e1567913929171439b15a7342ce23364fc1fa
EimonFarhad <eimonfarhad1996@gmail.com>
2019-04-29 03:33:50 +0530

    add vender details page

--- app/src/main/java/com/Myproject/myapp/Activity/VenderListingActivity.java
+++ app/src/main/java/com/Myproject/myapp/Activity/VenderListingActivity.java
@@ -1,18 +1,29 @@
 package com.Myproject.myapp.Activity;
 
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.Myproject.myapp.R;
 import com.Myproject.myapp.fragments.VenderLisingFragment;
 
-public class VenderListingActivity extends AppCompatActivity {
+public class VenderListingActivity extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout lin_near_me, lin_explore, lin_wish_list, lin_booking, lin_account;
+    ImageView img_near_me, img_explore, img_wish_list, img_booking, img_account;
+    TextView txt_near_me, txt_explore, txt_wish_list, txt_booking, txt_account;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,7 +32,7 @@ public class VenderListingActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_near:
-                    replace(new VenderLisingFragment());
+                    // replace(new VenderLisingFragment());
                     return true;
                 case R.id.navigation_explore:
 
@@ -53,6 +64,38 @@ public class VenderListingActivity extends AppCompatActivity {
         ft.replace(R.id.listing_container, new VenderLisingFragment()).commit();
         BottomNavigationView navigationView = findViewById(R.id.bottom_nev);
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //  initview();
+
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) navigationView.getChildAt(0);
+        for (int i = 0; i < menuView.getChildCount(); i++) {
+            final View iconView = menuView.getChildAt(i).findViewById(android.support.design.R.id.icon);
+            final ViewGroup.LayoutParams layoutParams = iconView.getLayoutParams();
+            final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 25, displayMetrics);
+            layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 25, displayMetrics);
+            iconView.setLayoutParams(layoutParams);
+        }
+
+    }
+
+    private void initview() {
+        lin_near_me = findViewById(R.id.lin_near_me);
+        lin_explore = findViewById(R.id.lin_explore);
+        lin_wish_list = findViewById(R.id.lin_wish_list);
+        lin_booking = findViewById(R.id.lin_booking);
+        lin_account = findViewById(R.id.lin_account);
+
+        img_near_me = findViewById(R.id.img_near_me);
+        img_explore = findViewById(R.id.img_explore);
+        img_wish_list = findViewById(R.id.img_wish_list);
+        img_booking = findViewById(R.id.img_booking);
+        img_account = findViewById(R.id.img_account);
+
+        txt_near_me = findViewById(R.id.txt_near_me);
+        txt_explore = findViewById(R.id.txt_explore);
+        txt_wish_list = findViewById(R.id.txt_wist_list);
+        txt_booking = findViewById(R.id.txt_booking);
+        txt_account = findViewById(R.id.txt_account);
 
     }
 
@@ -68,5 +111,10 @@ public class VenderListingActivity extends AppCompatActivity {
             ft.commit();
 
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/Myproject/myapp/fragments/VenderLisingFragment.java
+++ app/src/main/java/com/Myproject/myapp/fragments/VenderLisingFragment.java
@@ -30,22 +30,22 @@ import com.Myproject.myapp.R;
 
 import java.util.ArrayList;
 
-public class VenderLisingFragment extends Fragment implements View.OnClickListener{
-RecyclerView recycler_new_add,beach_item;
-ImageView filter;
+public class VenderLisingFragment extends Fragment implements View.OnClickListener {
+    RecyclerView recycler_new_add, beach_item;
+    ImageView filter;
 
-RelativeLayout change_event;
+    RelativeLayout change_event;
 
-RelativeLayout rel_change_event;
-ArrayList<NewAddedlistModel>arrayList;
-    ArrayList<Modelbeachitem>arrayList2;
-ImageButton change_service;
-TextView service_name;
+    RelativeLayout rel_change_event;
+    ArrayList<NewAddedlistModel> arrayList;
+    ArrayList<Modelbeachitem> arrayList2;
+    ImageButton change_service;
+    TextView service_name;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_vender_listing,container,false);
+        return inflater.inflate(R.layout.fragment_vender_listing, container, false);
     }
 
     @Override
@@ -54,44 +54,31 @@ TextView service_name;
         initview(view);
     }
 
-    private void initview(View view){
-        recycler_new_add =view.findViewById(R.id.recycler_new_add);
-        filter=view.findViewById(R.id.filter);
-        rel_change_event=view.findViewById(R.id.rel_change_event);
-        change_service=view.findViewById(R.id.change_service);
-        service_name=view.findViewById(R.id.service_name);
+    private void initview(View view) {
+        recycler_new_add = view.findViewById(R.id.recycler_new_add);
+        filter = view.findViewById(R.id.filter);
+        rel_change_event = view.findViewById(R.id.rel_change_event);
+        change_service = view.findViewById(R.id.change_service);
+        service_name = view.findViewById(R.id.service_name);
 
         beach_item = view.findViewById(R.id.beachimg_item);
-        filter=view.findViewById(R.id.filter);
-        change_event=view.findViewById(R.id.rel_change_event);
-        change_event.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               final Dialog dialog = new Dialog(getContext());
-                dialog.setContentView(R.layout.dialog_box);
-                dialog.show();
-            }
-        });
-        arrayList=new ArrayList<>();
-        alldata(arrayList);
-        arrayList2 =new ArrayList<>();
-        alldatabeach(arrayList2);
-        filter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              replace(new PriceRating());
-            }
-        });
+        filter = view.findViewById(R.id.filter);
+        change_event = view.findViewById(R.id.rel_change_event);
 
-        NewAddedAdapter adapter=new NewAddedAdapter(arrayList,getContext());
+        arrayList = new ArrayList<>();
+        alldata(arrayList);
+        arrayList2 = new ArrayList<>();
+        alldatabeach(arrayList2);
+
+        NewAddedAdapter adapter = new NewAddedAdapter(arrayList, getContext());
         recycler_new_add.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-recycler_new_add.setAdapter(adapter);
-filter.setOnClickListener(this);
+        recycler_new_add.setAdapter(adapter);
+        filter.setOnClickListener(this);
         rel_change_event.setOnClickListener(this);
         change_service.setOnClickListener(this);
 
         recycler_new_add.setAdapter(adapter);
-        BeachAdapter beachAdapter=new BeachAdapter(arrayList2,getContext());
+        BeachAdapter beachAdapter = new BeachAdapter(arrayList2, getContext());
         beach_item.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         beach_item.setAdapter(beachAdapter);
     }
@@ -106,12 +93,12 @@ filter.setOnClickListener(this);
     }
 
     private void alldata(ArrayList<NewAddedlistModel> arrayList) {
-        NewAddedlistModel model=new NewAddedlistModel();
+        NewAddedlistModel model = new NewAddedlistModel();
         model.setName("Eimon Farhad");
         model.setAddress("Sukanta nagar,Sector 4,pin-700098");
         model.setCount("20");
         arrayList.add(model);
-        NewAddedlistModel model1=new NewAddedlistModel();
+        NewAddedlistModel model1 = new NewAddedlistModel();
         model1.setName("Eimon Farhad");
         model1.setAddress("Sukanta nagar,Sector 4,pin-700098");
         model1.setCount("20");
@@ -120,7 +107,7 @@ filter.setOnClickListener(this);
 
     private void replace(Fragment fragment) {
         String backStateName = fragment.getClass().getName();
-        boolean fragmentPopped = getFragmentManager().popBackStackImmediate(backStateName,0);
+        boolean fragmentPopped = getFragmentManager().popBackStackImmediate(backStateName, 0);
 
         if (!fragmentPopped) {
             FragmentManager fm = getFragmentManager();
@@ -130,27 +117,30 @@ filter.setOnClickListener(this);
             ft.commit();
 
         }
-
+    }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.filter:
+                replace(new PriceRating());
 
                 break;
             case R.id.rel_change_event:
-
+                final Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.dialog_box);
+                dialog.show();
                 break;
             case R.id.change_service:
                 opendialog();
                 break;
-
         }
     }
 
-    private void opendialog(){
-        Dialog dialog=new Dialog(getContext());
+    private void opendialog() {
+        Dialog dialog = new Dialog(getContext());
         dialog.setContentView(R.layout.filter);
         dialog.show();
 
+    }
 }
