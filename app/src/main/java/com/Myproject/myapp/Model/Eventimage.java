07851728c3863f17b122abec8ba84c24403d8377
subhsis <subhasis391@gmail.com>
2019-05-04 22:31:32 +0530

    My message

---
+++ app/src/main/java/com/Myproject/myapp/Model/Eventimage.java
@@ -0,0 +1,19 @@
+package com.Myproject.myapp.Model;
+
+public class Eventimage {
+    int image;
+
+    public  Eventimage(int image){
+        this.image = image;
+
+    }
+
+    public int getImage() {
+
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
