0f1f6ff350dac9eee5f2104fc547840abfd2b811
subhsis <subhasis391@gmail.com>
2019-06-17 01:30:42 +0530

    registration2 modified

---
+++ app/src/main/java/com/Myproject/myapp/Model/EventcoverModel.java
@@ -0,0 +1,18 @@
+package com.Myproject.myapp.Model;
+
+public class EventcoverModel {
+    String CheckText;
+
+    public EventcoverModel(String checkText) {
+        CheckText = checkText;
+    }
+
+
+    public String getCheckText() {
+        return CheckText;
+    }
+
+    public void setCheckText(String checkText) {
+        CheckText = checkText;
+    }
+}
